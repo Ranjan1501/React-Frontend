ecc3689bcf8d9ffb49f5d6cec48357f17231db14
Ranjan1501 <ranjankumar1757@gmail.com>
2022-01-03 18:19:49 +0530

    C2

---
+++ Coding -C2/src/App.jsx
@@ -0,0 +1,14 @@
+// import logo from './logo.svg';
+import './App.css';
+import Recipe from "./components/Recipe"; 
+
+function App() {
+  return (
+    <div className="App">
+      <h3>Recipe Tracking Application</h3>
+      <Recipe />
+    </div>
+  );
+}
+
+export default App;
